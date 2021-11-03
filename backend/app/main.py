2a6ae229c037862a921b50972e00bbd845ea2cc0
meantint <mistinca@naver.com>
2021-11-03 11:21:41 +0900

    Create : API(signup), Session, cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,46 @@
 from typing import Optional
+from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
+import bcrypt, jwt
+import os
+
+from sqlalchemy.orm import Session
+from database import schemas, models, crud
+from database.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Cors
+origins = []
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/signup", status_code=200, response_model=schemas.User)
+async def signup(user_info: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    `회원가입 API`\n
+    :param user:s
+    :param db:
+    :return:
+    """
+    db_user = crud.get_user_by_userid(db, user_id=user_info.user_id)
+    if db_user:
+        raise HTTPException(status_code=400, detail="user_id already registered")
+    return crud.create_user(db=db, user=user_info)
