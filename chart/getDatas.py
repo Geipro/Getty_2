fa1202888c2eadc5d7ad182aaefa0e5dfa03d611
맹창영 <maengchang@gmail.com>
2021-11-03 16:53:02 +0900

    Comment : del comment

--- chart/getDatas.py
+++ chart/getDatas.py
@@ -2,6 +2,7 @@ import requests
 import json
 import math
 
+#url 호출을 위한 list
 check_list = []
 
 #coin 객체 = {'KRW-BTC' : {이름 : , 가격 : , 최고가(52주) : , 최저가(52주) : , 거래량(일): },     '이더리움' : {}} 과 같은 구조로 생긴 객체 생성
@@ -12,7 +13,6 @@ coin_object = dict()
 
 res = requests.get('https://api.upbit.com/v1/market/all').json()
 for li in res:
-    # print(str(li.values()))
     if 'KRW' in str(li.values()):
         coin_sym = li['market']
         coin_name = list(li.values())[1]
@@ -22,9 +22,6 @@ for li in res:
         check_list.append(coin_sym)
 
 
-# print(check_list)
-# print(coin_object)
-# print(json_obejct['market'])
 
 #coin_object 안에 데이터 넣기
 #얼마마다 갱신할 지
@@ -45,7 +42,3 @@ for coin in check_list[:1]:
     coin_object[coin]['lowest_52_week_price'] = low_pri
     coin_object[coin]['lowest_52_week_rate'] = round((pri - low_pri)/low_pri*100, 2)
     print(coin_object[coin])
-
-    # print(res)
-# res = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC').json()
-# print(res[0])
