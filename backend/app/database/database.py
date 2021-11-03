9547e88591821742dd760a6a123c9aa5ade1b8ed
meantint <mistinca@naver.com>
2021-11-03 11:21:11 +0900

    Create database.py

---
+++ backend/app/database/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(
+    "mysql+pymysql://k5a405:1234@k5a405.p.ssafy.io:3306/Project", convert_unicode=False
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
