bea2dfc9783c0b57deb9f30ce88d89b4a430ea9c
DESKTOP-KVCQHCD\multicampus <ragnaroik@naver.com>
2021-11-12 10:42:33 +0900

    news

--- front/front/src/views/News.vue
+++ front/front/src/views/News.vue
@@ -58,7 +58,7 @@ export default {
       //     console.log(error);
       //   });
       axios
-        .get('/news/' + encodeURI(this.searchQuery, 'utf-8'))
+        .get('https://k5a405.p.ssafy.io/backend/news/' + encodeURI(this.searchQuery, 'utf-8'))
         .then((response) => {
           this.newsList = response.data;
           console.log(this.newsList);
